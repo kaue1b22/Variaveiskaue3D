cda5ea55468da641054518e636cf4b5393053b45
kaue1b22 <128189073+kaue1b22@users.noreply.github.com>
2024-08-29 01:29:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let anoAtual = 2024;
+const nomeCompleto = "kaue henrique";
+let idade = 16;
+const cursoFaculdade = "TI";
+const cidadeAtual = "são josé dos pinhais";
+
+console.log (`ola eu sou o ${nomeCompleto} eu tenho ${idade} anos quero ano que vem estar cursando ${cursoFaculdade} e moro em ${cidadeAtual}`);
+
+anoAtual = 2025;
+
+console.log (anoAtual)
+
+idade = 17;
+
+console.log(`ola sou ${nomeCompleto} eu tenho ${idade} anos e estou cursando ${cursoFaculdade} e moro em ${cidadeAtual}`)
