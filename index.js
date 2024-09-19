3f92d2078d5f0bd0a790bcd491934524fb08e5bc
kaue1b22 <128189073+kaue1b22@users.noreply.github.com>
2024-09-19 01:38:13 +0000

    18/07/24

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ console.log (anoAtual)
 idade = 17;
 
 console.log(`ola sou ${nomeCompleto} eu tenho ${idade} anos e estou cursando ${cursoFaculdade} e moro em ${cidadeAtual}`)
+
+//agencia de viagens
+
+const nomeComprador = "diluc";
+const cpfComprador = 36911;
+const idadeComprador = 22
+const ListaDeDestinos = new Array(
+  "inazuma",
+  "sumero",
+  "fontaine",
+"natlan",
+"lyue",
+);
+
+console.log(`ola ${nomeComprador} do cpf ${cpfComprador} de idade ${idadeComprador} esta confirmada sua viagem para ${ListaDeDestinos} agredecemos a sua preferencia pela agencia de viagens paimon air lines`)
+
+console.log(ListaDeDestinos[1])
+console.log(`senhor ${nomeComprador} adicionamos mais um destino a sua viagem`)
+ListaDeDestinos.push("ghotam city")
