93b0026d3a736c10aa438d8b44d595debb2ff68d
kaue1b22 <128189073+kaue1b22@users.noreply.github.com>
2024-09-26 01:33:20 +0000

    25/09/24

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 let anoAtual = 2024;
 const nomeCompleto = "kaue henrique";
 let idade = 16;
@@ -16,9 +19,9 @@ console.log(`ola sou ${nomeCompleto} eu tenho ${idade} anos e estou cursando ${c
 
 //agencia de viagens
 
-const nomeComprador = "diluc";
-const cpfComprador = 36911;
-const idadeComprador = 22
+var nomeComprador = entrada("qual é seu nome?")
+var cpfComprador = entrada("qual é seu cpf?");
+var idadeComprador = entrada("qual sua idade?");
 const ListaDeDestinos = new Array(
   "inazuma",
   "sumero",
@@ -27,8 +30,16 @@ const ListaDeDestinos = new Array(
 "lyue",
 );
 
-console.log(`ola ${nomeComprador} do cpf ${cpfComprador} de idade ${idadeComprador} esta confirmada sua viagem para ${ListaDeDestinos} agredecemos a sua preferencia pela agencia de viagens paimon air lines`)
+console.log(`ola ${nomeComprador} do cpf ${cpfComprador} de idade ${idadeComprador} esta confirmada sua viagem para ${ListaDeDestinos} agredecemos a sua preferencia pela agencia de viagens paimon air lines`);
 
-console.log(ListaDeDestinos[1])
-console.log(`senhor ${nomeComprador} adicionamos mais um destino a sua viagem`)
-ListaDeDestinos.push("ghotam city")
+console.log(ListaDeDestinos[1]);
+console.log(`senhor ${nomeComprador} adicionamos mais um destino a sua viagem`);
+ListaDeDestinos.push("ghotam city");
+
+if (idadeComprador<18){
+  console.log(`infelizmente não podemos lhe vender uma passagem pois o senhor/a é menor de idade`)
+};
+
+if (idadeComprador>=18){
+  console.log(`parabens nós pudemos lhe vender uma passagem, estes são nossos destinos ${ListaDeDestinos}`) 
+};
